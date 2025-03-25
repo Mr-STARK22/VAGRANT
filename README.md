a37489502aebfe0e565e2582801fba07247a68d6
Bothkhalibre <vannaboth.meng@khalibre.com>
2025-03-25 13:26:49 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Vagrant command
+
+#### To start vagrant 
+```
+vagrant up 
+```
+#### To stop vagrant
+```
+vagrant suspend
+```
+#### To resume vagrant
+```
+vagrant resume
+```
+#### To provison vagrant
+```
+vagrant provision
+```
+#### SSH to vagrant
+```
+vagrant ssh
+```
+#### To reload vagrant provision
+```
+vagrant reload --provison
+```
+#### List vagrant box
+```
+vagrant box list
+```
+#### Remove box 
+```
+vagrant box remove <box-name>
+```
---
+++ ubuntu-vagrant/install_saltstack.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ensure keyrings dir exists
+mkdir -p /etc/apt/keyrings
+# Download public key
+curl -fsSL https://packages.broadcom.com/artifactory/api/security/keypair/SaltProjectKey/public | sudo tee /etc/apt/keyrings/salt-archive-keyring.pgp
+# Create apt repo target configuration
+curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.sources | sudo tee /etc/apt/sources.list.d/salt.sources
+
+echo 'Package: salt-*
+Pin: version 3006.*
+Pin-Priority: 1001' | sudo tee /etc/apt/preferences.d/salt-pin-1001
+
+sudo apt-get install salt-master
+sudo apt-get install salt-minion
+sudo apt-get install salt-ssh
+sudo apt-get install salt-syndic
+sudo apt-get install salt-cloud
+sudo apt-get install salt-api
+
+sudo systemctl enable salt-master && sudo systemctl start salt-master
+sudo systemctl enable salt-minion && sudo systemctl start salt-minion
+sudo systemctl enable salt-syndic && sudo systemctl start salt-syndic
+sudo systemctl enable salt-api && sudo systemctl start salt-api
